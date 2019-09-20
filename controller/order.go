77ec8c0d0c1d712333b2a67963208646a6a88dd4
Aditia Pratama <aditiapratamagg@gmail.com>
2019-09-20 16:13:49 +0700

    fix order controller route

--- controller/order.go
+++ controller/order.go
@@ -1,11 +1,23 @@
 package controller
 
 import (
-	"log"
 	"magento-consumer-service/domain"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func (c *controller) OrderManagement(consume *domain.Consume) {
-	// log.Println(db)
+	switch domain := consume.Data.Head.Domain; domain {
+	case "order":
+		switch action := consume.Data.Head.Action; action {
+		case "update":
+			err := c.OrderService.UpdateStatusOrder(consume)
+			if err != nil {
+				log.Error(err)
+			}
+		default:
+			log.Fatal("wrong action input")
+		}
+	}
 	log.Println("order")
 }
